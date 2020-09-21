685385a94097614ce19ed644a8f706f274c295f3
Boluwatife2904 <sanvicola2000@gmail.com>
2020-09-21 10:24:13 +0100

    Added Favicon to Pages and changed map coordinates

--- js/google-map.js
+++ js/google-map.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location coordinates
-  var location = { lat: 9.05177, lng: 7.46564 };
+  var location = { lat: 41.213181, lng: -124.004631 };
   // The map
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
+    zoom: 10,
     center: location,
   });
   // The marker, positioned at Uluru
