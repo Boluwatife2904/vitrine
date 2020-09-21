1b80b1a643a7a6752777bc04e5dd3519934d5dcf
Boluwatife2904 <sanvicola2000@gmail.com>
2020-09-21 03:00:41 +0100

    Cleaning Up

--- js/google-map.js
+++ js/google-map.js
@@ -1,14 +1,14 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var uluru = { lat: 9.05177, lng: 7.46564 };
-  // The map, centered at Uluru
+  // The location coordinates
+  var location = { lat: 9.05177, lng: 7.46564 };
+  // The map
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-    center: uluru,
+    center: location,
   });
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: uluru, map: map });
+  var marker = new google.maps.Marker({ position: location, map: map });
 }
 
 initMap();
