d47fdb197a423e544e01ec249759d3aa830ed1c7
Boluwatife2904 <sanvicola2000@gmail.com>
2020-09-21 01:34:41 +0100

    Added Desktop Search Functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+$(function () {
+  $('a[href="#search"]').on('click', function(event) {
+      event.preventDefault();
+      $('#search').addClass('open');
+      $('#search > form > input[type="search"]').focus();
+  });
+  
+  $('#search, #search button.close').on('click keyup', function(event) {
+      if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+          $(this).removeClass('open');
+      }
+  });
+});
+
 const menuBtn = document.querySelector(".menu-icon button");
 const cartBtn = document.querySelector(".cart-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
